3061a644c3a010d603c62dfd56b06ff026cfeb83
Kakeru YAMAMORI <e205754@ie.u-ryukyu.ac.jp>
2021-11-13 01:50:49 +0900

    1st commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("main method runs");
+        Chatbot bot1 = new Chatbot();
+        Chatbot bot2 = new Chatbot("caviar");
+        bot1.greeting();
+        bot2.greeting();
+        bot2.name = "キャビア";
+        bot2.greeting();
+    }
+    
+}
